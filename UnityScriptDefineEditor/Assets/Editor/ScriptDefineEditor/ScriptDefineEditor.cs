db6f3647c434faee0abcdfc6d2b9b51582e8538b
Suodesz <suodesz@outlook.com>
2020-10-03 21:39:40 +0800

    Create editor script

---
+++ UnityScriptDefineEditor/Assets/Editor/ScriptDefineEditor/ScriptDefineEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace Suodesz.ScriptDefineEditor
+{
+    public class ScriptDefineEditor : EditorWindow
+    {
+        [MenuItem("Window/Script Define Editor %#w")]
+        static void CreatMenu()
+        {
+            var editor = EditorWindow.GetWindow(typeof(ScriptDefineEditor), true, "Define Editor");
+            editor.Show();
+        }
+    }
+}
